91b8699f774b9a0d37003784faa7294e3f6ab069
Igor Shahin <Igor.Shahin22310@outlook.com>
2018-11-21 21:39:02 +0300

    ffd

---
+++ app/src/main/java/com/example/igor/androidtask/KategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.igor.androidtask;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class KategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_layout);
+    }
+}
--- app/src/main/java/com/example/igor/androidtask/MainActivity.java
+++ app/src/main/java/com/example/igor/androidtask/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.igor.androidtask;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 public class MainActivity extends Activity {
@@ -10,7 +12,16 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, KategoryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5*1000);
 
     }
 }
